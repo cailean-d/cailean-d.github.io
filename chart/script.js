1ad32a4e60390d34310e98c65ce2deeecd83d7a3
Denis Lykov <cailean393@gmail.com>
2019-04-05 04:17:17 +0300

    1

--- chart/script.js
+++ chart/script.js
@@ -1,3 +1,12 @@
+// 1. ширина столбца + (сss 7 строка)
+// 2. цвет столбца + (css 2 строка)
+// 3. шаг + (js 18 строка)
+// 4. сужался и расширялся в зависимости от кол-ва столбцов +
+// 5. max-width подписи по горизонтали
+// 6. процент графика от макс значения столбцов + (js 19 строка)
+// 7. высота и ширина графика + (css 11, 12 строка)
+// 8. адаптивная ширина графика + 
+
 // данные
 var data = [
     1, 1, 11, 16, 11, 17, 16, 20, 1, 1, 11, 16, 11, 17, 16, 20, 1, 1, 11, 16, 11, 17, 16, 20, 16, 20
@@ -6,21 +15,8 @@ var data = [
 // подпись
 var label = '2 мес. 2013 г.';
 
-var width = 1200;       // ширина графика
-var height = 500;       // высота графика
-var step = 5;           // шаг
-var high = 0.55;        // коеффициент высоты
-
-
-
-// 1. толщина столбца + (сss)
-// 2. цвет столбца + (css)
-// 3. шаг
-// 4. сужался и расширялся в зависимости от кол-ва столбцов 
-// 5. max-width подписи по горизонтали
-// 6. процент графика от макс значения столбцов
-// 7. высота и ширина графика
-// 8. адаптивная ширина графика + 
+var step = 5;                   // шаг
+var highModifier = 0.55;        // коеффициент высоты
 
 new Chartist.Bar('.ct-chart', {
     labels: labels(),
@@ -30,7 +26,7 @@ new Chartist.Bar('.ct-chart', {
         distributeSeries: true,
         width: '100%',
         // height: '100%',
-        high: Math.max.apply(null, data) / high,
+        high: Math.max.apply(null, data) / highModifier,
         low: 0,
         seriesBarDistance: 12,
         axisY: {
@@ -51,7 +47,7 @@ function labels() {
 
 function steps() {
     var x = [0];
-    while (x[x.length - 1] <= Math.max.apply(null, data) / high) {
+    while (x[x.length - 1] <= Math.max.apply(null, data) / highModifier) {
         x.push(x[x.length - 1] + step)
     }
     return x;
