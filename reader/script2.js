e7ed8dc8870e6c021ebdb7cbe1a807c0a5a2c380
Denis Lykov <cailean393@gmail.com>
2019-04-13 20:09:34 +0300

    1

--- reader/script2.js
+++ reader/script2.js
@@ -1,5 +1,20 @@
-fetch('https://cors.io/https://reader-backend1.herokuapp.com/pages/1')
+fetch('https://reader-backend1.herokuapp.com/pages/1')
 .then((res) => {
 	console.log(res.text())
 })
 .catch(console.log)
+
+
+let xhr = new XMLHttpRequest();
+
+xhr.open('get', 'https://reader-backend1.herokuapp.com/pages/1');
+
+xhr.send();
+
+xhr.onloadend =  function() {
+    console.log(xhr.responseText)
+}
+
+xhr.onerror = function() {
+    console.log(xhr.status)
+}
