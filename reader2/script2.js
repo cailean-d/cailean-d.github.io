7a788ba934f31cc080aa99aaaacecf22cef2b1e5
Denis Lykov <cailean393@gmail.com>
2019-04-13 22:13:57 +0300

    1

--- reader2/script2.js
+++ reader2/script2.js
@@ -1,16 +1,24 @@
-function loadPages(...pages) {
+let isLoading = false;
+
+function loadPages(cb, ...pages) {
     pages.forEach(i => {
         fetch('pages/' + i + '.html').then(res => res.text().then(text => $(readerSelector).append(text)))
     })
+    if (typeof cb == 'function') {
+        cb();
+    }
 }
 
 
-loadPages(1, 2, 3, 4, 5)
+loadPages(null, 1, 2, 3, 4, 5)
 
 
 window.onscroll = function() {
     let el = document.documentElement;
     if ((el.clientHeight + el.scrollTop) >= el.scrollHeight) {
-        console.log('bottom');
+        if (!isLoading) {
+            isLoading = true;
+            loadPages(_ => isLoading = false, 6, 7, 8, 9, 10)
+        }
     }
 };
