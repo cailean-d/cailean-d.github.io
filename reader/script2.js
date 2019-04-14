7a3f9dc3ebd83c09e83b3ccb25618ce7eeca1876
Denis Lykov <cailean393@gmail.com>
2019-04-14 06:15:29 +0300

    1

--- reader/script2.js
+++ reader/script2.js
@@ -21,7 +21,7 @@ function loadPages(pages, dir) {
                 })
             } else {
                 $('.loader-wrapper').remove();
-                setTimeout(_ => isLoading = false, 500);                
+                setTimeout(_ => isLoading = false, 1000);                
                 if (dir) {
                     bottomIsLoaded = true;
                     setCookie('last-page', bottomPage - pageCount * 2, { expires: 2592000 });
@@ -31,7 +31,7 @@ function loadPages(pages, dir) {
             }
         });
     }  else {
-        setTimeout(_ => isLoading = false, 500);
+        setTimeout(_ => isLoading = false, 1000);
         $('.loader-wrapper').remove();
         if (dir) {
             $(readerSelector).append(data)
