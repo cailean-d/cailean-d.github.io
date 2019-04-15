73cabfbff3561013f8c442e36723aaf14acdaa43
Denis Lykov <cailean393@gmail.com>
2019-04-15 17:25:27 +0300

    1

--- reader/script2.js
+++ reader/script2.js
@@ -121,10 +121,10 @@ $(document).ready(function() {
         var scrollTop = $(window).scrollTop();
         if (scrollTop > stickyNavTop) { 
             $($stickyMenu).addClass('sticky');
-            $('body').css('padding-top', navHeight + 'px')
+            $('.reader').css('padding-top', navHeight + 'px')
         } else {
             $($stickyMenu).removeClass('sticky');
-            $('body').css('padding-top', '0')
+            $('.reader').css('padding-top', '0')
         }
     };
 
