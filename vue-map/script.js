8782ccfb1d0d62aa899846bf2d13fe4acb5ff6d0
Denis Lykov <cailean393@gmail.com>
2019-05-18 16:08:54 +0300

    1

--- vue-map/script.js
+++ vue-map/script.js
@@ -1,32 +1,85 @@
-let container = document.getElementById('popup');
-let content = document.getElementById('popup-content');
-let closer = document.getElementById('popup-closer');
-
-
 Vue.component('vue-map', {
-    template: '<div ref="map" class="map"></div>',
+    template: ` <div>
+                    <div ref="map" class="map"></div>
+                    <div ref="popup" class="ol-popup">
+                        <a href="#" ref="popupCloser" class="ol-popup-closer"></a>
+                        <div ref="popupContent"></div>
+                    </div>
+                </div>`,
     data: () => ({
-        map: null
+        map: null,
+        overlay: null,
+        textColor: '#559060',
+        textHoverColor: '#f00',
+        startCoords: [46.0676732, 41.3403794],
+        zoom: 5,
+        minZoom: 5,
+        maxZoom: 19,
+        angleRotation: 45,
+        markerText: 'rotate',
+        markerSource: 'marker.png',
+        markerScale: 0.5
     }),
     mounted() {
         this.initialize();
     },
     methods: {
         initialize() {
-            let overlay = new ol.Overlay({
-                element: container,
+            this.initOverlay();
+            this.initMap();
+            this.loadMarkers();
+
+            this.map.on('singleclick', e => {
+                let hit = false;
+
+                this.map.forEachFeatureAtPixel(e.pixel, (feature, layer) => {
+                    hit = true;
+                    if (feature.get('id') == 'marker') {
+                        this.showOverlay(feature);
+                    } else if (feature.get('id') == 'text') {
+                        this.rotateMarker(layer);
+                    }
+                })
+
+                if (!hit) {
+                    this.setMarket(e.coordinate);
+                }
+
+            });
+
+            this.map.on("pointermove", e => {
+                this.resetTextColor();
+                let hit = this.map.forEachFeatureAtPixel(e.pixel, (feature, layer) => {
+                    if (feature.get('id') == 'text') {
+                        this.setTextColor(feature, this.textHoverColor);
+                    }
+                    return true;
+                }); 
+
+                if (hit) {
+                    this.map.getTarget().style.cursor = 'pointer';
+                } else {
+                    this.map.getTarget().style.cursor = '';
+                }
+            });
+
+        },
+        initOverlay() {
+            this.overlay = new ol.Overlay({
+                element: this.$refs.popup,
                 autoPan: true,
                 autoPanAnimation: {
                   duration: 250
                 }
-              });
+            });
 
-              closer.onclick = function() {
-                overlay.setPosition(undefined);
-                closer.blur();
+            this.$refs.popupCloser.onclick = () => {
+                this.overlay.setPosition(undefined);
+                this.$refs.popupCloser.blur();
                 return false;
-              };
-
+            };
+        },
+        initMap() {
             this.map = new ol.Map({
                 target: this.$refs.map,
                 layers: [
@@ -35,68 +88,13 @@ Vue.component('vue-map', {
                     })
                 ],
                 view: new ol.View({
-                    center: ol.proj.fromLonLat([46.0676732, 41.3403794]),
-                    zoom: 4
+                    center: ol.proj.fromLonLat(this.startCoords),
+                    zoom: this.zoom,
+                    minZoom: this.minZoom,
+                    maxZoom: this.maxZoom
                 }),
-                overlays: [overlay],
+                overlays: [this.overlay],
             });
-
-            this.map.on('singleclick', e => {
-                let hitMarkers = false;
-
-                this.map.forEachFeatureAtPixel(e.pixel, (feature, layer) => {
-                    hitMarkers = true;
-
-                    if (feature.get('id') == 'marker') {
-                        let pixel = this.map.getPixelFromCoordinate(feature.getGeometry().getCoordinates());
-                        pixel[1] -= 35;
-                        let point = this.map.getCoordinateFromPixel(pixel);
-                        content.innerHTML = '<p>Some text</p>';
-                        overlay.setPosition(point);
-                    } else {
-                        var features = layer.getSource().getFeatures();
-
-                        for (var i in features) {
-                            var feature = features[i];
-                            if (feature.get('id') == 'marker') {
-                                let
-                                    style = feature.getStyle(),
-                                    image = style.getImage(),
-                                    rotation = image.getRotation();
-                                
-                                image.setRotation(rotation + 5);
-                                feature.setStyle(style);
-                            }
-                        }
-                        
-                    }
-                    
-
-                   
-                })
-
-                if (!hitMarkers) {
-                    let coords = ol.proj.transform(e.coordinate, 'EPSG:3857', 'EPSG:4326');
-                    this.drawMarker({lng: coords[0], lat: coords[1]})
-                }
-
-            });
-
-
-            this.map.on("pointermove", function (evt) {
-                var hit = this.forEachFeatureAtPixel(evt.pixel, function(feature, layer) {
-                    return true;
-                }); 
-                if (hit) {
-                    this.getTarget().style.cursor = 'pointer';
-                } else {
-                    this.getTarget().style.cursor = '';
-                }
-            });
-
-            this.loadMarkers();
-
-
         },
         drawMarker(marker) {
             let features = [];
@@ -107,48 +105,32 @@ Vue.component('vue-map', {
             let markerFeature = new ol.Feature({
                 geometry: new ol.geom.Point(coords)
             });
-
             markerFeature.set('id', 'marker');
 
-            // console.log(coords)
-
-            // let pixel = this.map.getPixelFromCoordinate(coords);
-            // pixel[1] += 1;
-            // let point = this.map.getCoordinateFromPixel(pixel);
-
-            // coords[1] -= 10000;
+            let markerStyle = new ol.style.Style({
+                image: new ol.style.Icon(({
+                    anchor: [0.5, 0.5],
+                    src: this.markerSource,
+                    rotation: marker.angle * (Math.PI/180),
+                    scale: this.markerScale
+                }))
+            });
 
             let textFeature = new ol.Feature({
                 geometry: new ol.geom.Point(coords)
             });
-
             textFeature.set('id', 'text');
-
-            // markerFeature.set('name', 'marker')
-            // markerFeature.set('h', latitude)
-            // markerFeature.set('v', longitude)
-
-            let markerStyle = new ol.style.Style({
-                image: new ol.style.Icon(({
-                    anchor: [0.5, 1],
-                    src: "https://cdn.mapmarker.io/api/v1/pin?text=P&size=35&hoffset=1",
-                    rotation: 0 * (Math.PI/180) 
-                }))
-            });
-
 
             let textStyle = new ol.style.Style({
                 text: new ol.style.Text({
                     font: 'bold 12px Calibri,sans-serif',
-                    fill: new ol.style.Fill({ color: '#559060' }),
+                    fill: new ol.style.Fill({ color: this.textColor }),
                     stroke: new ol.style.Stroke({
                       color: '#fff', width: 2
                     }),
-                    // get the text from the feature - `this` is ol.Feature
-                    // and show only under certain resolution
-                    text: 'rotate',
-                    offsetY: 5
-                  })
+                    text: this.markerText,
+                    offsetY: 20
+                })
             })
         
             markerFeature.setStyle(markerStyle);
@@ -161,6 +143,56 @@ Vue.component('vue-map', {
 
             this.map.addLayer(vectorLayer);
         },
+        showOverlay(feature) {
+            let point = this.addPixelsToCoords(feature.getGeometry().getCoordinates(), -10);
+            this.$refs.popupContent.innerHTML = '<p>Some text</p>';
+            this.overlay.setPosition(point);
+        },
+        setMarket(coordinate) {
+            let coords = ol.proj.transform(coordinate, 'EPSG:3857', 'EPSG:4326');
+            let marker = {lng: coords[0], lat: coords[1], angle: 0};
+            console.log('new marker: ', marker.lng, marker.lat, marker.angle);
+            this.drawMarker(marker);
+        },
+        rotateMarker(layer) {
+            let features = layer.getSource().getFeatures();
+            features.forEach(i => {
+                if (i.get('id') == 'marker') {
+                    let style = i.getStyle();
+                    let image = style.getImage();
+                    let rotation = image.getRotation();
+                    let newRotation = rotation +  this.angleRotation * (Math.PI/180);
+                    let newAngle = Math.round((newRotation * (180/Math.PI)) % 360);
+                    console.log('new rotation: ' + newAngle)
+                    image.setRotation(newRotation);
+                    i.setStyle(style);
+                }
+            });
+        },
+        setTextColor(feature, color) {
+            let style = feature.getStyle();
+            let text = style.getText();
+            text.setFill(new ol.style.Fill({ color: color }));
+            feature.setStyle(style);
+        },
+        resetTextColor() {
+            let layers = this.map.getLayers().getArray();
+            layers.forEach(i => {
+                if (i.type == 'VECTOR') {
+                    let features = i.getSource().getFeatures();
+                    features.forEach(i => {
+                        if (i.get('id') == 'text') {
+                            this.setTextColor(i, this.textColor);
+                        }
+                    })
+                }
+            })
+        },
+        addPixelsToCoords(coords, pixels) {
+            let pixel = this.map.getPixelFromCoordinate(coords);
+            pixel[1] = pixel[1] + pixels;
+            return this.map.getCoordinateFromPixel(pixel);
+        },
         loadMarkers() {
             fetch('markers.json')
             .then(res => {
@@ -171,11 +203,9 @@ Vue.component('vue-map', {
                     this.drawMarker(i)
                 })
             })
-        }
- 
+        },
     }
 })
-
 
 
 let app = new Vue({
