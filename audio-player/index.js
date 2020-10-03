e43b55db97d16aa97678d2985d9812a055a909ab
Denis Lykov <cailean393@gmail.com>
2020-10-03 18:33:43 +0300

    1

--- audio-player/index.js
+++ audio-player/index.js
@@ -1,103 +1,72 @@
-var options = {
-  controls: [
-    "play-large",
-    "play",
-    "progress",
-    "current-time",
-    "mute",
-    "volume",
-  ]
-};
-var tracks = [
-  {
-    name: "Test1 - Testtesttestestesteststeststeststest",
-    duration: "4:29",
-    src: "http://retro-disko.ru/6/music/016.mp3"
-  },
-  {
-    name: "Test2 - Testtesttestestesteststeststeststest",
-    duration: "4:29",
-    src: "http://retro-disko.ru/6/music/016.mp3"
-  },
-  {
-    name: "Test3 - Testtesttestestesteststeststeststest",
-    duration: "4:29",
-    src: "http://retro-disko.ru/6/music/016.mp3"
-  },
-  {
-    name: "Test4 - Testtesttestestesteststeststeststest",
-    duration: "4:29",
-    src: "http://retro-disko.ru/6/music/016.mp3"
-  },
-  {
-    name: "Test - Test",
-    duration: "4:29",
-    src: "http://retro-disko.ru/6/music/016.mp3"
-  },
-  {
-    name: "Test - Test",
-    duration: "4:29",
-    src: "http://retro-disko.ru/6/music/016.mp3"
-  },
-  {
-    name: "Test - Test",
-    duration: "4:29",
-    src: "http://retro-disko.ru/6/music/016.mp3"
-  },
-  {
-    name: "Test - Test",
-    duration: "4:29",
-    src: "http://retro-disko.ru/6/music/016.mp3"
-  },
-  {
-    name: "Test - Test",
-    duration: "4:29",
-    src: "http://retro-disko.ru/6/music/016.mp3"
-  },
-  {
-    name: "Test - Test",
-    duration: "4:29",
-    src: "http://retro-disko.ru/6/music/016.mp3"
-  },
-  {
-    name: "Test - Test",
-    duration: "4:29",
-    src: "http://retro-disko.ru/6/music/016.mp3"
-  },
-  {
-    name: "Test - Test",
-    duration: "4:29",
-    src: "http://retro-disko.ru/6/music/016.mp3"
-  },
-  {
-    name: "Test - Test",
-    duration: "4:29",
-    src: "http://retro-disko.ru/6/music/016.mp3"
-  },
-];
+function AudioPlayer(selector, playlist) {
 
-function isScrolledIntoView(elem, container) {
-  var containerTop = $(container).scrollTop();
-  var containerBottom = containerTop + $(container).get(0).clientHeight;
-
-  var elemTop = $(elem).get(0).offsetTop - $(container).get(0).offsetTop;
-  var elemBottom = elemTop + $(elem).get(0).clientHeight;
-
-  return ((elemBottom <= containerBottom) && (elemTop >= containerTop));
-}
-
-$(function() {
+  var dblclicked = 0;
+  var tracks = playlist || [];
   var supportsAudio = !!document.createElement("audio").canPlayType;
+  var options = {
+    controls: [
+      "play-large",
+      "play",
+      "progress",
+      "current-time",
+      "mute",
+      "volume",
+    ]
+  };
+
+  var $playerContainer = 
+  $('<div class="player-container">'+
+    '<div class="wrap">' +
+      '<div class="player-title"></div> ' +
+      '<div class="controlsWrap">' +
+        '<div><audio data-audio preload>Your browser does not support HTML5 Audio!</audio></div>' +
+        '<div class="playListButtons">' +
+          '<a class="playListButtons__prev"><i class="glyphicon glyphicon-backward"></i></a>' +
+          '<a class="playListButtons__edit" data-toggle="popoverE"><i class="glyphicon glyphicon-edit"></i></a>' +
+          '<a class="playListButtons__next"><i class="glyphicon glyphicon-forward"></i></a>' +
+        '</div>' +
+      '</div>' +
+      '<ul class="playerList"></ul>' +
+      '<button class="forwardBtn" style="display: none;">' +
+        '<span class="glyphicon glyphicon-fast-forward" aria-hidden="true"></span>' +
+      '</button>' +
+    '</div>' +
+  '</div>');
+
+  function isScrolledIntoView(elem, container) {
+    var containerTop = $(container).scrollTop();
+    var containerBottom = containerTop + $(container).get(0).clientHeight;
+
+    var elemTop = $(elem).get(0).offsetTop - $(container).get(0).offsetTop;
+    var elemBottom = elemTop + $(elem).get(0).clientHeight;
+
+    return ((elemBottom <= containerBottom) && (elemTop >= containerTop));
+  }
+
+  var setDblClicked = function() {
+    dblclicked = 2;
+  }
+
+  var preventDlbClicked = function(callback) {
+    var self = this;
+    var args = [].slice.call(arguments, 1);
+    setTimeout(function() {
+      if (dblclicked) return dblclicked--;
+      callback.apply(self, args);
+    }, 300);
+  }
+
   if (supportsAudio) {
-    var player = new Plyr("#audio", options);
+    $(selector).html($playerContainer);
+    var audio = $playerContainer.find('[data-audio]').get(0);
+    var player = new Plyr(audio, options);
     var index = 0;
     var trackCount = tracks.length;
-    var playerTitle = $(".player-title");
-    var audio = $("#audio").get(0);
+    var playerTitle = $playerContainer.find(".player-title");
 
     var updateList = function(id) {
-      $(".playerListItem--selected").removeClass("playerListItem--selected");
-      $(".playerList li:eq(" + id + ")").addClass("playerListItem--selected");
+      $playerContainer.find(".playerListItem--selected").removeClass("playerListItem--selected");
+      $playerContainer.find(".playerList li:eq(" + id + ")").addClass("playerListItem--selected");
     }
 
     var updateTitle = function(id) {
@@ -130,12 +99,18 @@ $(function() {
 
     var loadPlaylist = function (list) {
       $.each(list, function(index, track) {
-        $(".playerList").append(
+        $playerContainer.find(".playerList").append(
           '<li>' + 
             '<div class="playerListItem">' +
               '<span class="playerListItem__number">' + fillNumber(index + 1) + '.</span>' +
               '<span class="playerListItem__title">' + track.name + '</span>' +
+              '<div class="playerListItem__titleWrap">' +
+                '<span class="playerListItem__titleWrap__author"></span>' + 
+                '<span> - </span>' + 
+                '<input type="text" class="form-control playerListItem__titleWrap__t">' + 
+              '</div>' +
               '<span class="playerListItem__length">' + track.duration + '</span>' +
+              '<input type="text" class="form-control playerListItem__length-editable">' + 
             '</div>' +
           '</li>'
         );
@@ -143,8 +118,9 @@ $(function() {
     };
 
     var scrollList = function(dir) {
-      var $selected = $(".playerListItem--selected");
-      if (!isScrolledIntoView($selected, ".playerList")) {
+      var $selected = $playerContainer.find(".playerListItem--selected");
+      var $playlist = $playerContainer.find(".playerList");
+      if (!isScrolledIntoView($selected, $playlist)) {
         $selected.get(0).scrollIntoView(dir);
       }
     }
@@ -166,32 +142,154 @@ $(function() {
     var rewindToLast10Sec = function() {
       player.currentTime = player.duration - 10;
     }
-
-    var focusPlayer = function() {
-      $(".plyr").get(0).focus();
+    
+    var resizeList = function() {
+      var list = $playerContainer.find(".playerList").get(0);
+      var h0 = parseInt(getComputedStyle(list).maxHeight);
+      var h = window.innerHeight - list.getBoundingClientRect().y - 60;
+      if (h > h0) list.style.maxHeight = h + 'px';
     }
-      
-    $(".playListButtons__prev").on("click", prevTrack);
-    $(".playListButtons__next").on("click", nextTrack);
-    $('.forwardBtn').click(rewindToLast10Sec);
-    $("#audio").on("ended", function() { nextTrack(); audio.play(); });
+
+    var clearTitleEditable = function($item) {
+      // change visible items
+      $item.find(".playerListItem__titleWrap").hide();
+      $item.find(".playerListItem__title").show();
+
+      // clear inputs
+      $item.find(".playerListItem__titleWrap__author").text('');
+      $item.find(".playerListItem__titleWrap__t").val('');
+    }
+
+    var clearLengthEditable = function($item) {
+      // change visible items
+      $item.find(".playerListItem__length-editable").hide();
+      $item.find(".playerListItem__length").show();
+
+      // clear inputs
+      $item.find(".playerListItem__length-editable").val('');
+    }
+
+    var sendChanges = function() {
+      var data = JSON.stringify(tracks);
+      alert(data);
+
+      // changes is saved, hide the button
+      $playerContainer.find(".playListButtons__edit").removeClass('playListButtons__edit-show');
+    }
+
+
+    $playerContainer.find(".playListButtons__prev").on("click", prevTrack);
+    $playerContainer.find(".playListButtons__next").on("click", nextTrack);
+    $playerContainer.find(".playListButtons__edit").on("click", sendChanges);
+    $playerContainer.find('.forwardBtn').click(rewindToLast10Sec);
+    $(audio).on("ended", function() { nextTrack(); audio.play(); });
 
     player.on("ready", function() {
-      $('.plyr__controls > button:first-child').after($('.forwardBtn').show());
-      $('.player-container .wrap').css('display', 'flex');
-      focusPlayer();
-      $('.player-container').on('focus click', focusPlayer);
+      $forwardBtn = $playerContainer.find('.forwardBtn').show();
+      $playerContainer.find('.plyr__controls > button:first-child').after($forwardBtn);
+      $playerContainer.find('.wrap').css('display', 'flex');
+      resizeList();
+
+      // activate popover for edit button
+      $('[data-toggle="popoverE"]').popover({ 
+        title: "Warning!",
+        content: "List is edited. Press the button to save changes.",
+        trigger: "hover",
+        placement: "top",
+      });
     });
     
-    $(".playerList").on("click", "li", function() {
-      var id = +$(this).index();
-      if (id !== index) playTrack(id);
+    $playerContainer.find(".playerList").on("click", "li", function(e) {
+      preventDlbClicked.call(this, function(target) {
+        var isTitleInput = $(target).hasClass("playerListItem__titleWrap__t");
+        var isLengthInput = $(target).hasClass("playerListItem__length-editable");
+        if (isTitleInput || isLengthInput) return;
+        var id = +$(this).index();
+        if (id !== index) playTrack(id);
+      }, e.target);
     });
+
+    $playerContainer.find(".playerList").on("dblclick", ".playerListItem__title", function() {
+      setDblClicked();
+      var $item = $(this).closest(".playerListItem");
+      $item.find(".playerListItem__titleWrap").css("display", "flex");
+      $item.find(".playerListItem__title").hide();
+      $item.find(".playerListItem__titleWrap__t").focus();
+
+      // set author
+      var author = $item.find(".playerListItem__title").text().split('-')[0].trim();
+      $item.find(".playerListItem__titleWrap__author").text(author);
+    });
+
+    $playerContainer.find(".playerList").on("keypress", ".playerListItem__titleWrap__t", function(e) {
+      // if pressed enter
+      if (e.keyCode === 13) {
+        var $item = $(this).closest(".playerListItem");
+        var item_index = +$item.parent().index();
+
+        // clear and exit if input is empty
+        if (!this.value) return clearTitleEditable($item);
+
+        // get data 
+        var author = $item.find('.playerListItem__titleWrap__author').text();
+        var title = $(this).val();
+        
+        // change item
+        var newName = author + ' - ' + title;
+        tracks[item_index].name = newName;
+        $item.find(".playerListItem__title").text(newName);
+
+        // update title if current playing item
+        if (index === item_index) { 
+          updateTitle(index);
+        }
+
+        // items if edited, show send button
+        $playerContainer.find(".playListButtons__edit").addClass('playListButtons__edit-show');
+
+        clearTitleEditable($item);
+      }
+    });
+
+    $playerContainer.find(".playerList").on("dblclick", ".playerListItem__length", function() {
+      setDblClicked();
+      var $item = $(this).closest(".playerListItem");
+      $item.find(".playerListItem__length").hide();
+      $item.find(".playerListItem__length-editable").show().focus();
+
+      // fill input with prev duration
+      var len = $item.find(".playerListItem__length").text();
+      $item.find(".playerListItem__length-editable").val(len);
+    });
+
+    $playerContainer.find(".playerList").on("keypress", ".playerListItem__length-editable", function(e) {
+      // if pressed enter
+      if (e.keyCode === 13) {
+        var $item = $(this).closest(".playerListItem");
+        var item_index = +$item.parent().index();
+        
+        // clear and exit if input is empty
+        if (!this.value) return clearLengthEditable($item);
+
+        // get data 
+        var len = $(this).val();
+        
+        // change item
+        tracks[item_index].duration = len;
+        $item.find(".playerListItem__length").text(len);
+
+        // items if edited, show send button
+        $playerContainer.find(".playListButtons__edit").addClass('playListButtons__edit-show');
+
+        clearLengthEditable($item);
+      }
+
+    })
 
     loadPlaylist(tracks);
     loadTrack(index);
   } else {
-    var noSupport = $("#audio").text();
-    $(".player-container").append('<p class="no-support">' + noSupport + "</p>");
+    var noSupport = $(audio).text();
+    $playerContainer.find(".player-container").append('<p class="no-support">' + noSupport + "</p>");
   }
-});
+}
